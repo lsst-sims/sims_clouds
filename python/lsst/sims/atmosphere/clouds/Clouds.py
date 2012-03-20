6a5449d1e5600c37b3eda2df235d84e39da562c6
ljones <ljones@git.lsstcorp.org>
2012-03-20 01:01:52 +0000

    Working to understand clouds and powerspectrum; added comments.

--- python/lsst/sims/atmosphere/clouds/Clouds.py
+++ python/lsst/sims/atmosphere/clouds/Clouds.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-from pylab import *
-from numpy import *
-
+import numpy
 from scipy import interpolate, fftpack
-#from pylab import imshow
 
 ## get clean ...
 
@@ -11,27 +7,27 @@ class Clouds:
     
     def __init__(self, ws, s):
         # set the half-diagonal size of the 2D Fourier space equal to the maximum frequency of the 1D power spectrum
-        self.windowsize = ws/sqrt(2.)
+        self.windowsize = ws/numpy.sqrt(2.)
         # set the sampling directly in the real space : same sampling than the correlation function (arbitrary)
         self.sampling = s
         # and the spacing
-        self.xstep = self.windowsize/float(self.sampling)
+        self.xstep = self.windowsize/numpy.float(self.sampling)
         
     def DirectClouds(self, correl2D):
         """Directly compute clouds using random noise in real space"""
         # nothing to do with symetry, quarters shift, etc.
         # compute 2D power spectrum from 2D symetric correlation function
-        PowerSpec2D = abs(fftpack.fft2(correl2D))
-        # get a 2D random gaussien noise with rms = 1
-        noise2D = random.normal(zeros(self.sampling*self.sampling), 1.).reshape(self.sampling, self.sampling)
+        PowerSpec2D = numpy.abs(fftpack.fft2(correl2D))
+        # get a 2D random gaussian noise with rms = 1
+        noise2D = numpy.random.normal(zeros(self.sampling*self.sampling), 1.).reshape(self.sampling, self.sampling)
         # a realization is given in Fourier space calculating tf(noise)*sqrt(powerspectum)
-        fourierclouds = fftpack.fft2(noise2D)*sqrt(PowerSpec2D)
+        fourierclouds = fftpack.fft2(noise2D)*numpy.sqrt(PowerSpec2D)
         # then, inverse Fourier transform to get clouds
-        self.clouds = real(fftpack.ifft2(fourierclouds))
+        self.clouds = numpy.real(fftpack.ifft2(fourierclouds))
 
 
     def WriteClouds(self, filename):
-        """write clouds in a text file x y z"""
+        """Write clouds in a text file x y z"""
         f = open(filename, 'w')
         # only take 1/4 of the 2D real space (other 3/4 is only use for symetry of the correlation function)
         # but 4 fields of view can be taken separatly from one realization
@@ -41,10 +37,13 @@ class Clouds:
                 f.write(str(i*self.xstep)+'\t'+str(j*self.xstep)+'\t'+str(real(self.clouds[i,j]))+'\n')
         f.close()
 
+"""
 ## essai
     def FitClouds(self):
-        """fit clouds with a 2D spline"""
+        # fit clouds with a 2D spline
         # build vectors
+        x = numpy.arange(0, self.sampling/2, self.xstep)
+        
         x=[]
         y=[]
         z=[]
@@ -84,3 +83,5 @@ class Clouds:
 ##         colorbar()
 ##         show()
         stop=raw_input()
+
+"""
--- python/lsst/sims/atmosphere/clouds/PowerSpectrum.py
+++ python/lsst/sims/atmosphere/clouds/PowerSpectrum.py
@@ -1,47 +1,58 @@
 # -*- coding: utf-8 -*-
-from numpy import *
+import numpy
 from scipy import interpolate
 
 class PowerSpectrum:
     
     def __init__(self, ws, s):
+        """Instantiate the PowerSpectrum object to hold the structure function for cloud variance.
+        
+        The windowsize (ws) and sampling rate (s) for the structure function are set here.
+        The window size details the total size of the structure function (so set to be slightly
+        larger than or equivalent to the fov). The sampling size details how often the power
+        spectrum is sampled to create the structure function .. higher # means sharper variations."""
         self.windowsize = ws
         self.sampling = s
 
-    def ComputeStructureFunction(self):
-        """create structure function based on an analytical model (fitted on data)"""
+    def ComputeStructureFunction(self, x0=0.0, x1=1.75, y1=0.04, ymm=0.08):
+        """Create the structure function based on an analytical model.
+
+        This analytical model is determined by fitting data (done elsewhere).
+        The variables x0, x1, y1, and ymm describe the structure function.
+        x0=0, x1=1.75,y1=0.04, ymm=0.08 are defaults fit to the SDSS (?) structure function.
+        ymm is related to the long-range grayscale extinction, while x1 and y2 are related to
+        the inflection points in the structure function. """
         # define the x range
-        self.xstep = self.windowsize/float(self.sampling)
-        x = []
-        for i in range(self.sampling):
-            x.append(i*self.xstep)
-        self.x = array(x)
-        # useful variables
-        x0 = 0.
-        x1 = 1.75
-        y1 = 0.04
-        ymm = 0.08
+        self.xstep = self.windowsize/numpy.float(self.sampling)
+        self.x = numpy.arange(0, self.sampling-self.xstep, self.xstep)
+        # Calculate variable for the structure function.
         al = -(1/(x1-x0))*log(1-(y1/ymm))
-        # structure function
+        # Calculate the actual structure function.
         self.SF = ymm*(1.-exp(-al*self.x))   
         
     def ComputeCorrelationFunction(self):
-        """compute the correlation function from the structure function"""
+        """Compute the correlation function from the structure function."""
         # following the definition astro-ph/0703157v1
-        tmp = -.5*self.SF**2
-        self.correl = -min(tmp)+tmp
+        tmp = -0.5*self.SF**2
+        self.correl = -numpy.min(tmp)+tmp
+        # Is this actually used anywhere other than in writing out the correlation function?
 
     def getCorrel2D(self):
         """return a 2D correlation function"""
+        # Okay, bit a hack here .. not sure why this method & ComputeStructureFunction should be separate if no separate arguments
+        try:
+            self.xstep
+        except AttributeError:
+            self.ComputeStructureFunction()            
         # 1D windowssize correspond to 2D diagonal
-        localxstep = self.xstep/sqrt(2.)
+        localxstep = self.xstep/numpy.sqrt(2.)
         tmp = -.5*self.SF**2
-        correltmp = -min(tmp)+tmp
+        correltmp = -numpy.min(tmp)+tmp
         # interpolation of the correlation function on 2D real space
         interpfunc = interpolate.interp1d(self.x, correltmp, bounds_error=False, fill_value = 0.)
         # 2D sampling = 1D sampling (arbitrary)
-        correl2D = zeros(self.sampling*self.sampling).reshape(self.sampling, self.sampling)
-        # symetry for the 2D correlation function
+        correl2D = numpy.zeros(self.sampling*self.sampling).reshape(self.sampling, self.sampling)
+        # symmetry for the 2D correlation function
         # to define a periodic function
         # correl[size-i][size-j] = correl[i][j]
         # correl[i][size-j] = correl[i][j]
