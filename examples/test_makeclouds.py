1f8aef68bf8fd286d7d26fd96037f58fbd7bfc02
ljones <ljones@git.lsstcorp.org>
2012-06-12 15:50:06 +0000

    updated test_makeclouds

--- examples/test_makeclouds.py
+++ examples/test_makeclouds.py
@@ -9,12 +9,9 @@ def make_clouds(sampling=256, seed=42):
     # Set up the power spectrum for the clouds.
     rad_fov = 1.8  #degrees? or radians?
     windowsize = numpy.sqrt(2.*(2*rad_fov)**2)
-    # and 'for fft symmetry double the windowsize'
-    #  (not sure why this is needed)
-    windowsize *= 2
     # Really only have to set up PS once, since we're not changing
     # any of the parameters.  (so save correlfun & reuse)
-    ps = PowerSpectrum.PowerSpectrum(windowsize, sampling)
+    ps = PowerSpectrum.PowerSpectrum(windowsize, sampling)  
     ps.ComputeStructureFunction()
     correlfunc = ps.getCorrel2D()
     clouds = Clouds.Clouds(windowsize, sampling)
